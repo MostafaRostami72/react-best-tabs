ee7c375af463978c9e5a4c84a886bc8571c1c72f
mostafa <m.rostamimahmodi72@gmail.com>
2020-08-07 19:02:32 +0430

    Updates

---
+++ precache-manifest.09c6b556d8e4f8b2e54472ef4c2cced3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "088e3f8efaef16f557ab2c6ac202f86d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "de7b46e8164105495a0e",
+    "url": "./static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "108b69a2f7df872c77b8",
+    "url": "./static/js/2.c55acd73.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.c55acd73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de7b46e8164105495a0e",
+    "url": "./static/js/main.9d0cfe69.chunk.js"
+  },
+  {
+    "revision": "5cbacb2c7c43676ab852",
+    "url": "./static/js/runtime-main.7b2dd6f3.js"
+  }
+]);
